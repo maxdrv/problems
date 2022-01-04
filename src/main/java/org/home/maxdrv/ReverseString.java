ec9e03ca57fd035c1b5c6b08c7eaa9b1d5c3bcc4
maxdrv <maxdrv.stud@gmail.com>
2022-01-04 23:16:24 +0300

    solution for reverse string problem

---
+++ src/main/java/org/home/maxdrv/ReverseString.java
@@ -0,0 +1,22 @@
+package org.home.maxdrv;
+
+/**
+ * https://leetcode.com/problems/reverse-string/
+ */
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+}
