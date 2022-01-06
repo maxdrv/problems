6e4ff0908f8c774759cdd5bf8efd03d5fc15ad4f
maxdrv <maxdrv.stud@gmail.com>
2022-01-06 23:40:33 +0300

    added explanation for selection sort algorithm

--- src/main/java/org/home/maxdrv/sort/SelectionSort.java
+++ src/main/java/org/home/maxdrv/sort/SelectionSort.java
@@ -1,5 +1,20 @@
 package org.home.maxdrv.sort;
 
+/**
+ * move over array of N elements
+ *
+ * on each move find the lowest element from elements
+ * place the lowest element on first position and element from first position to position of lowest element
+ *
+ * repeat over N elements
+ * sub step to find the lowest element requires N, N-1, N-2, ... , 1 iterations
+ * so N, N-1, N-2, ... , 1 = (N + 1) / 2
+ *
+ * O(N * (N + 1) / 2)
+ *
+ * or just O(N^2)
+ *
+ */
 public class SelectionSort implements Sort {
 
     @Override
