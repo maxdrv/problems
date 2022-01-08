fea7667076a65f113a215917aa92917e91d79446
maxdrv <maxdrv.stud@gmail.com>
2022-01-08 23:04:51 +0300

    annotation @TestFunction. Reason: reduce boilerplate code for parametrized tests

---
+++ src/test/java/org/home/maxdrv/util/TestFunctions.java
@@ -0,0 +1,16 @@
+package org.home.maxdrv.util;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ParameterizedTest
+@MethodSource("functions")
+public @interface TestFunctions {
+}
